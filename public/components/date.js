0a21aedaada63f77b88859fb74fcb00a4009a6ea
benjaminxia <26benjaminx@students.harker.org>
2023-02-13 10:51:29 -0800

    removed redundant files

--- public/components/date.js
+++
@@ -1,8 +0,0 @@
-month = new Date().toLocaleString('default', { month: 'short' });
-day = new Date().getDate();
-year = new Date().getFullYear();
-
-
-function GetDate() {
-    document.getElementById("getDate").innerText = `${month} ${day}, ${year}`;
-}
